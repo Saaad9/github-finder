815f65019b0a8ae18fe712b19fa5e27ab39b0868
saaad99 <ehdgnstla@naver.com>
2024-11-30 21:52:25 +0900

    [add] cell 선택시 하이라이트 효과

--- spread-sheet-app/script.js
+++ spread-sheet-app/script.js
@@ -8,12 +8,14 @@ const alphabets =
 ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 class Cell {
-    constructor(isHeader, diabled, data, row, column, active = false) {
+    constructor(isHeader, diabled, data, row, column, rowName, columnName, active = false) {
         this.isHeader = isHeader;
         this.disabled = diabled;
         this.data = data;
         this.row = row;
         this.column = column;
+        this.rowName = rowName;
+        this.columnName = columnName;
         this.active = active;
     }
 }
@@ -25,22 +27,37 @@ function initSpreadsheet() {
         let spreadSheetRow = [];
         for (let j = 0; j < COLS; j++) {
             let cell_data = "";
+            let is_header = false;
+            let disabeld = false;
+            let rowName = i;
+            let columnName = alphabets[j-1];
+            
             // 행의 첫번째 요소일 때 숫자 삽입
             if (j === 0) {
                 cell_data = i;
+                is_header = true;
+                disabeld = true;
             }
             // 첫 번째 행일 때 숫자 삽입
             if (i === 0) {
                 // 알파벳 매칭 시켜야함
                 cell_data = alphabets[j-1];
+                is_header = true;
+                disabeld = true;
             }
 
             // [0,0] 데이터 x
             if (i === 0  && j === 0) {
                 cell_data = "";
+                is_header = true;
+                disabeld = true;
             }
 
-            const cell = new Cell(false, false, cell_data, i, j, false);
+            if (columnName === undefined) {
+                columnName = "0";
+            }
+            
+            const cell = new Cell(is_header, disabeld, cell_data, i, j, rowName, columnName, false);
             spreadSheetRow.push(cell);
         }
         spreadSheet.push(spreadSheetRow);
@@ -54,9 +71,41 @@ initSpreadsheet();
 function createCellElm (cell) { 
     const cellEl = document.createElement('input'); 
     cellEl.className = 'cell'; 
-    cellEl.id = 'cell_' + cell.row + cell.column; 
+    cellEl.id = cell.rowName + cell.columnName; 
     cellEl.value = cell.data;
     cellEl.disabled = cell.disabled;
+
+    if(cell.isHeader) {
+        cellEl.classList.add("header");
+    }
+
+    cellEl.addEventListener('click',() =>{
+        // console.log(cell.rowName + " - " + cell.columnName); 
+        const selectRow = cell.rowName;
+        const selectColumn = cell.columnName;
+
+        const selectRowElm = document.getElementById("0" + selectColumn);
+        const selectColumnElm = document.getElementById(selectRow + "0");
+        
+        selectRowElm.classList.add("selected");
+        selectColumnElm.classList.add("selected");
+        
+        cellEl.classList.add("active");
+
+        
+    });
+
+    cellEl.addEventListener('blur', () => {
+        cellEl.classList.remove("active");
+        const selectRow = cell.rowName;
+        const selectColumn = cell.columnName;
+
+        const selectRowElm = document.getElementById("0" + selectColumn);
+        const selectColumnElm = document.getElementById(selectRow + "0");
+
+        selectRowElm.classList.remove("selected");
+        selectColumnElm.classList.remove("selected");
+    });
 
     return cellEl;
 }
